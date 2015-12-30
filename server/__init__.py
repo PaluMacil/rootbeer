21fe70910b36befa080fcd8f8b619208c7270fcb
Palu Macil <dcwolf@gmail.com>
2015-12-30 00:42:54 -0500

    added a question example

--- server/__init__.py
+++ server/__init__.py
@@ -5,7 +5,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app = Flask(__name__)
+app = Flask(__name__.split('.')[0])
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 db = SQLAlchemy(app)
 
